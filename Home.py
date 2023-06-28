2d43d41ba7eb0e5934c8d989e80e3c63f3f64f49
HIEUNT1610 <trung-hieu.ngo@etu.u-paris.fr>
2023-06-28 22:23:48 +0200

    moving visualization

--- Home.py
+++ Home.py
@@ -145,6 +145,25 @@ def scrape_webpages(urls):
     return df
 
 # Visualisation functions: just use BERTopic's visualisation functions
+# Functions for visualizations. This is for caching the results so they don't have to be loaded each time:
+@st.cache_data
+def visualize_topics_fake():
+    return topic_model_fake.visualize_topics(top_n_topics=100, custom_labels= True, width=700, height=700)
+@st.cache_data
+def visualize_topics_true():
+    return topic_model_true.visualize_topics(top_n_topics=100, custom_labels= True, width=700, height=700)
+@st.cache_data
+def visualize_hierarchy_fake():
+    return topic_model_fake.visualize_hierarchy(top_n_topics=30, custom_labels=True, width=700, height=700)
+@st.cache_data
+def visualize_hierarchy_true():
+    return topic_model_true.visualize_hierarchy(top_n_topics=30, custom_labels=True, width=700, height=700)
+@st.cache_data
+def get_topic_info_fake():
+    return topic_model_fake.get_topic_info().head(30)
+@st.cache_data
+def get_topic_info_true():
+    return topic_model_true.get_topic_info().head(30)
 
 from requests.exceptions import MissingSchema
 
@@ -185,3 +204,25 @@ if st.button("Predict"):
             st.dataframe(documents)
     else:
         st.write("Please upload pdf files and/or enter urls.")
+        
+# In here we are just going to show the visualizations of the topics. We will use the same models as in the Home.py file.
+st.subheader("Visualizations of the trained topics:")
+st.markdown("Which type of visualization would you like to see?")
+option1 = st.selectbox("Select model:", (" ", "Fake news", "True news"))
+option2 = st.selectbox("Select visualization:", (" ", "Top 30 topics", "Topic distribution", "Topic hierarchy"))
+
+# Visualizations:
+if option1 == "Fake news" and option2 == "Topic distribution":
+    st.write(visualize_topics_fake())
+elif option1 == "Fake news" and option2 == "Top 30 topics":
+    st.write(get_topic_info_fake())
+elif option1 == "Fake news" and option2 == "Topic hierarchy":
+    st.write(visualize_hierarchy_fake())
+elif option1 == "True news" and option2 == "Topic distribution":
+    st.write(visualize_topics_true())
+elif option1 == "True news" and option2 == "Top 30 topics":
+    st.write(get_topic_info_true())
+elif option1 == "True news" and option2 == "Topic hierarchy":
+    st.write(visualize_hierarchy_true())
+else:
+    st.write("Please select a model and a visualization from the dropdown menus above.")
