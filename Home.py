d9822022959956ec0ab79f564b88e2f63834a2d3
HIEUNT1610 <118187999+HIEUNT1610@users.noreply.github.com>
2023-11-30 10:38:51 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,238 +1,49 @@
-# Getting started
-from bertopic.representation import MaximalMarginalRelevance
-from sklearn.feature_extraction.text import CountVectorizer
-from sentence_transformers import SentenceTransformer
-from bs4 import BeautifulSoup
-from bertopic import BERTopic
-from hdbscan import HDBSCAN
-from pypdf import PdfReader
-from io import BytesIO
-from umap import UMAP
-import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
-import pickle
-import gdown
-import re
-import os
+# Fake News Detection using BERTopic
 
-# Title and Layout:
-st.set_page_config(page_title="Fake news detection using BERTopic", layout="wide")
-st.title("Fake news detection using BERTopic")
-st.header("""
-            A simple demo to detect if a given document or a web article belongs to a common fake news topic or not.""")
+## Overview
 
-st.markdown("""        
-            This demo app is based on BERTopic, a topic modeling technique that leverages BERT embeddings and c-TF-IDF to create dense clusters allowing for easily interpretable topics whilst keeping important words in the topic descriptions. The topics for the input documents are generated based on the content using a combination of topic modelling techniques, and are compared to the topics of fake news and true news from the MisInfo Kaggle dataset. The detection is based on the similarity between the input document and the fake news topics, using a simple decision tree using the ground truth provided by the dataset. 
-            
-            This demo app's purpose is to: (i) provide a quick detection tool for further analysis; (ii) demonstrate my ability in looking at a problem and creating a solution from end to end. For more details, please refer to the About page.
-            """)
-st.subheader("How to use this app:")  
-st.markdown("""  
-            Users can simply upload PDF files or input the URLs of web articles to start detection.
-            Please ensure that URLs are not behind paywalls, or the app will not be able to access the content.
-            """)
+This repository presents a simple demonstration of fake news detection using topic modelling. The demo utilizes BERTopic, a topic modelling technique that harnesses BERT embeddings and c-TF-IDF to form dense clusters, facilitating easily interpretable topics while retaining crucial words in topic descriptions.
 
-# Function for models loading:
-@st.cache_resource(ttl=3600) # Cache the model so it doesn't have to be loaded each time. Limit to 1 hour.
-def download_and_cache_models():
-    """Download pre-trained models from Google Drive.
-    This function returns 2 BERTopic models, one trained on fake news and one trained on true news.
-    The models were trained on Misinfo dataset from Kaggle, based on EUvsDisinfo data.
-    Models are cached so they don't have to be downloaded each time."""
-    #gdown.download(id = "1XJfCt7PFm0LlZBMDKJF-9BvukG8Pj0Yo", output = "misinfo-fake-pickle", quiet=False)
-    #gdown.download(id = "1Bt7LDObSscall84N344uwkXhJIxXfsHZ", output = "misinfo-true-pickle", quiet=False)  
-        
-    # Load models. Loading without embedding made things worse, but it's not possible to do otherwise with streamlit sharing:
-    sentence_model = SentenceTransformer("paraphrase-multilingual-mpnet-base-v2")
-    #topic_model_fake = BERTopic.load("misinfo-fake.pickle", embedding_model= sentence_model)
-    #topic_model_true = BERTopic.load("misinfo-true.pickle", embedding_model= sentence_model)
-    #sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-    topic_model_fake = BERTopic.load("misinfo-fake-minilm.pickle", embedding_model= sentence_model)
-    topic_model_true = BERTopic.load("misinfo-true-minilm.pickle", embedding_model= sentence_model)
+## Purpose
 
-    return topic_model_fake, topic_model_true, sentence_model
+The primary goal of this demo is two-fold:
 
-# Functions for reading pdf files:
-from io import BytesIO
-def read_pdf(uploaded_file):
-    reader = PdfReader(BytesIO(uploaded_file.read()))
-    text = ""
-    
-    for page in range(len(reader.pages)):
-        page_text = reader.pages[page].extract_text()
-        # Remove headers and footers
-        page_text = re.sub(r'\d+/\d+/\d+, \d+:\d+ [AP]M.*?https?://.*?/\d+/\d+', '', page_text)
-        # Remove page numbers
-        page_text = re.sub(r'Page \d+ of \d+', '', page_text)
-        # Remove links
-        page_text = re.sub(r'https?://\S+', '', page_text)
-        
-        text += page_text + "\n"
+1. **Detection Tool:** Offer a tool for quickly identifying potentially fake news articles, paving the way for further in-depth analysis.
+  
+2. **Showcase Skills:** Demonstrate proficiency in addressing a problem and developing a comprehensive solution from end to end.
 
-    return text
+## Approach
 
-def extract_text_from_pdfs(uploaded_files):
-    df = pd.DataFrame(columns=["file", "text"])
-    for uploaded_file in uploaded_files:
-        # Read the PDF file
-        text = read_pdf(uploaded_file)
-        # Add the file name and the text to the data frame
-        name = uploaded_file.name
-        name = re.sub('.pdf', '', name)
-        df = df.append({"file": name, "text": name + ' ' + text}, ignore_index=True)
-    return df
+The core of the demo relies on comparing the topic of a given document or news article with the topics generated by two BERTopic models trained on fake and true news datasets. If the input document's topic aligns with the topics from the fake news dataset, it raises a flag for potential falsity, urging a more thorough verification process. While not a flawless solution, this method provides a rapid means to sift through numerous documents and articles, pinpointing those most likely to be fake.
 
+## Model Details
 
-# Function for prediction:
-def predict(documents, topic_model_fake, topic_model_true, sentence_model):
-    """This function takes in a list of pdf files and prints out the predicted topics based on the trained models.
-    documents: a dataframe with 2 columns: name and text
-    topic_model_fake: the trained topic model for fake news, imported from pickle
-    topic_model_true: the trained topic model for true news, imported from pickle
-    sentence_model: the trained sentence transformer model"""
-    
-    # Vectorize the documents
-    test_embeddings = sentence_model.encode(documents["text"].astype(str).tolist())
-    
-    # Perform topic predictions on the documents
-    test_topics_true, test_probs_true = topic_model_true.transform(documents['text'].astype(str).tolist(), test_embeddings)
-    test_topics_fake, test_probs_fake = topic_model_fake.transform(documents['text'].astype(str).tolist(), test_embeddings)
-    
-    # Print out predictions:
-    documents["prediction"] = ""
-    result_str = []
-    for i in range(len(documents['text'].tolist())):
-        # if both models cannot predict the document
-        if not topic_model_true.get_topic(test_topics_true[i]) and not topic_model_fake.get_topic(test_topics_fake[i]):
-            documents["prediction"][i] = "Not in fake news topics"
-            result_str.append("Model could not predict reliably if the document is true or fake based on training data.\n")
-        
-        # if predicted topic in true model but not in fake model    
-        elif topic_model_true.get_topic(test_topics_true[i]) and not topic_model_fake.get_topic(test_topics_fake[i]):
-            documents["prediction"][i] = "Not in fake news topics"            
-            result_str.append(f"In topic {topic_model_true.topic_labels_[test_topics_true[i]]} at {test_probs_true[i][test_topics_true[i]]*100:0.4f}%.\n")
-        
-        # if predicted topic in fake model but not in true model    
-        elif topic_model_fake.get_topic(test_topics_fake[i]) and not topic_model_true.get_topic(test_topics_true[i]):
-            documents["prediction"][i] = "In common fake news topics"
-            result_str.append(f"In the topic {topic_model_fake.topic_labels_[test_topics_fake[i]]} at {test_probs_fake[i][test_topics_fake[i]]*100:0.4f}%.\n")
-        
-        # if predicted topic in both models
-        else:
-            if test_probs_true[i][test_topics_true[i]] > test_probs_fake[i][test_topics_fake[i]]:
-                documents["prediction"][i] = "Not in fake news topics"
-                result_str.append(f"In the topic {topic_model_true.topic_labels_[test_topics_true[i]]} at {test_probs_true[i][test_topics_true[i]]*100:0.4f}%.\n")
-            else:
-                documents["prediction"][i] = "In common fake news topics"
-                result_str.append(f"In the topic {topic_model_fake.topic_labels_[test_topics_fake[i]]} at {test_probs_fake[i][test_topics_fake[i]]*100:0.4f}%.\n")
-    
-    return result_str
+The BERTopic models utilized in this demo were trained on the Misinfo dataset from Kaggle, derived from EUvsDisinfo data, using miniLM Sentence Transformer embedding. The combination of unsupervised learning techniques and labelled data enables effective fake news detection. The prediction process involves generating the input topic and comparing it to the topics clustered by the models based on the two datasets.
 
+## Versions
 
-# Function for scraping webpages:
-def scrape_webpages(urls):
-    """Scrape the contents of a list of webpages and return a DataFrame"""
-    data = []
-    for url in urls:
-        response = requests.get(url)
-        if response.status_code != 200:
-            # Attempt to bypass paywall using 12ft.io
-            try:
-                response = requests.get(f'https://api.12ft.io/v1/extract?url={url}')
-                text = response.json().get('data', {}).get('text', '')
-            except:
-                text = ''
-        else:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            text = soup.get_text()
-        name = url.split('/')[-1]
-        name = re.sub("[-_.]", ' ', name)
-        data.append({'file': name, 'text': name + ' ' + text})
-    df = pd.DataFrame(data)
-    return df
+Two versions of the demo are available:
 
-# Visualisation functions: just use BERTopic's visualisation functions
-# Functions for visualizations. This is for caching the results so they don't have to be loaded each time:
-@st.cache_data
-def visualize_topics_fake():
-    return topic_model_fake.visualize_topics(top_n_topics=100, custom_labels= True, width=700, height=700)
-@st.cache_data
-def visualize_topics_true():
-    return topic_model_true.visualize_topics(top_n_topics=100, custom_labels= True, width=700, height=700)
-@st.cache_data
-def visualize_hierarchy_fake():
-    return topic_model_fake.visualize_hierarchy(top_n_topics=30, custom_labels=True, width=700, height=700)
-@st.cache_data
-def visualize_hierarchy_true():
-    return topic_model_true.visualize_hierarchy(top_n_topics=30, custom_labels=True, width=700, height=700)
-@st.cache_data
-def get_topic_info_fake():
-    return topic_model_fake.get_topic_info().head(30)
-@st.cache_data
-def get_topic_info_true():
-    return topic_model_true.get_topic_info().head(30)
+1. **Streamlit Version (Small):** Yields reasonable results, though the potential for improvement exists due to limited hosting space on the Streamlit Community Cloud.
 
-from requests.exceptions import MissingSchema
+2. **Main Version (Large):** Provides superior and more easily interpreted topics, showing promise. Enhanced detection capabilities can be achieved with larger embedding models (e.g., paraphrase-multilingual-mpnet-base-v2). Continuous performance enhancement is possible by augmenting the training set with more data. A deployment on Google Cloud Service for the larger version will be accessible soon.
 
-# Scripts for the app:
-# Loading models:
-with st.spinner("Loading model. Please read descriptions in the meantime..."):
-    topic_model_fake, topic_model_true, sentence_model = download_and_cache_models()
-documents = pd.DataFrame(columns=["file", "text"])
+## How to Use
 
-st.subheader("File uploader / URL scraper")
-# File uploader:
-pdf_files = st.file_uploader("Upload PDF files", type=["pdf"],
-                               accept_multiple_files=True)
-if pdf_files: 
-    with st.spinner("Processing PDF files..."): 
-        documents = extract_text_from_pdfs(pdf_files)
-    
-# Text input for urls:
-urls = st.text_input("Enter URLs, separated by commas")
-if urls:
-    urls = [url.strip() for url in urls.split(',')]
-    try:
-        with st.spinner("Scraping webpages…"):
-            url_documents = scrape_webpages(urls)
-    except MissingSchema as e:
-        st.error(f"Invalid URL: {e}. Please enter a valid URL with a schema (e.g. http:// or https://).")
-    else:
-        documents = pd.concat([documents, url_documents], ignore_index=True)
+1. **Clone the Repository:**
+   ```
+   git clone https://github.com/HIEUNT1610/NLP---Fake-news-detection-using-BERTopic.git
+   cd NLP---Fake-news-detection-using-BERTopic
+   ```
 
-# Perform prediction:
-if st.button("Predict"):
-    if not documents.empty:
-        with st.spinner("Detecting. Please hold..."):
-            st.write("Detecting if documents are in common fake news topic or not: \n")
-            result_str = predict(documents, topic_model_fake, topic_model_true, sentence_model)
-            documents["result"] = result_str
-            documents = documents[["file", "prediction", "result"]]
-            st.dataframe(documents, use_container_width=True)
-            st.write("The results can be interpreted as: the document in the first column is detected to be likely to be in the fake news/non-fake news topic in the second column, with the predicted percentage listed out in the third column along with the actual predicted topic. \n")    else:
-        st.write("Please upload pdf files and/or enter urls.")
-        
-# In here we are just going to show the visualizations of the topics. We will use the same models as in the Home.py file.
-st.subheader("Visualizations of the trained topics:")
-st.markdown("In this section you can see the visualizations of the trained topics. The topics are generated from the fake news dataset and the true news dataset. The visualizations are made with the BERTopic package, and you can check Top 30 topics, Topic distribution, and Topic hierarchy.")
-st.markdown("Which type of visualization would you like to see?")
-option1 = st.selectbox("Select model:", (" ", "Fake news", "True news"))
-option2 = st.selectbox("Select visualization:", (" ", "Top 30 topics", "Topic distribution", "Topic hierarchy"))
+2. **Install Dependencies:**
+   ```
+   pip install -r requirements.txt
+   ```
 
-# Visualizations:
-if option1 == "Fake news" and option2 == "Topic distribution":
-    st.write(visualize_topics_fake())
-elif option1 == "Fake news" and option2 == "Top 30 topics":
-    st.write(get_topic_info_fake())
-elif option1 == "Fake news" and option2 == "Topic hierarchy":
-    st.write(visualize_hierarchy_fake())
-elif option1 == "True news" and option2 == "Topic distribution":
-    st.write(visualize_topics_true())
-elif option1 == "True news" and option2 == "Top 30 topics":
-    st.write(get_topic_info_true())
-elif option1 == "True news" and option2 == "Topic hierarchy":
-    st.write(visualize_hierarchy_true())
-else:
-    st.write("Please select a model and a visualization from the dropdown menus above.")
+3. **Run the Demo:**
+   ```
+   python -m streamlit run Home.py
+   ```
+
+This command will initiate the interactive interface, allowing users to explore and test the fake news detection capabilities.
